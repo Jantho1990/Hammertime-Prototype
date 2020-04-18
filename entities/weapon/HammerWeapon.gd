900644619b613debb0e3449e11378f99f8a405f7
Josh Anthony <jantho1990@gmail.com>
2020-04-18 18:20:13 -0500

    added hammer to player; added throw states

--- entities/weapon/HammerWeapon.gd
+++ entities/weapon/HammerWeapon.gd
@@ -1,10 +1,21 @@
 extends KinematicBody2D
 
+enum throw_state {
+  HOLDING,
+  THROWING,
+  SUSPENDING,
+  RETURNING
+}
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var current_throw_state = throw_state.HOLDING setget set_throw_state
 
+var hold_offset = Vector2(6, 0)
+var dir = Vector2(1, 0)
+var motion = Vector2(0, 0)
+
+var rotation_force_deg = 10
+
+onready var parent = get_parent()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +23,32 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#  pass
+func _physics_process(_delta):
+  dir = parent.direction
+  match current_throw_state:
+    throw_state.THROWING:
+      handle_throw_state_throwing()
+    throw_state.SUSPENDING:
+      handle_throw_state_suspending()
+    throw_state.RETURNING:
+      handle_throw_state_returning()
+    throw_state.HOLDING, _:
+     handle_throw_state_holding()
+
+func handle_throw_state_holding():
+  if rotation != 0:
+    rotation = 0
+  # position = parent.position + (hold_offset * dir)
+
+func handle_throw_state_throwing():
+  pass
+
+func handle_throw_state_suspending():
+  pass
+
+func handle_throw_state_returning():
+  pass
+
+func set_throw_state(value):
+  if throw_state.has(value):
+    current_throw_state = throw_state[value]
