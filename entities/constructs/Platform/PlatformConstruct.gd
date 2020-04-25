3ac7dd36f4fe75b9a9dc9f54a8e8ac574d3027e5
Josh Anthony <jantho1990@gmail.com>
2020-04-25 11:55:21 -0500

    ported PlatformGizmo into prototype and related systems, renamed it PlatformConstruct

---
+++ entities/constructs/Platform/PlatformConstruct.gd
@@ -0,0 +1,24 @@
+extends Construct
+
+class_name PlatformConstruct
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+  position.y += sin(global.run_time / 0.1) * 0.35
+  update()
+
+func _draw():
+  var ext = $CollisionArea/CollisionShape2D.shape.extents
+  var rec = Rect2(Vector2(0, 0) - Vector2(ext.x / 2, ext.y / 2), ext)
+  draw_rect(rec, Color(1, 1, 1))
---
+++ systems/deletable/Deletable.gd
@@ -0,0 +1,22 @@
+extends Node
+
+###
+# Dependent on:
+# - GlobalSignal
+# - a parent
+###
+
+onready var parent = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  GlobalSignal.listen('delete_unit', self, 'on_Delete_unit')
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func on_Delete_unit():
+  if Selection.selected_entity == parent: # or Selection.previously_selected_entity == parent:
+    Selection.clear_selection()
+    parent.queue_free()
