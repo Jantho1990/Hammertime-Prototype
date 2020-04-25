81b66a7438cc76664407071faa11918e18046d41
Josh Anthony <jantho1990@gmail.com>
2020-04-25 15:21:04 -0500

    fixing bugs with the spellcasting ui; added ReductionBar to SpellcasterEnergyBar

--- ui/SpellcasterEnergyBar.gd
+++ ui/SpellcasterEnergyBar.gd
@@ -1,16 +1,19 @@
-extends ProgressBar
+extends Control
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var EnergyBar
+var ReductionBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+  EnergyBar = $EnergyBar
+  ReductionBar = $ReductionBar
   GlobalSignal.listen('energy_change', self, '_on_Energy_change')
 
 
 func _on_Energy_change(data):
   var energy = data.energy
-  value = energy
+  EnergyBar.value = energy
