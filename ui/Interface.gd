8a10d3250c29bdf68e7cdb4660f6bd49c11afa72
Josh Anthony <jantho1990@gmail.com>
2020-04-25 16:01:00 -0500

    stole the unit selection code from Rabbit Trails and added it to Interface

---
+++ ui/Interface.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_LEFT:
+			if Selection.has_selection():
+				Selection.clear_selection()
+		elif event.button_index == BUTTON_RIGHT:
+			if Selection.has_selection():
+				GlobalSignal.dispatch('cancel_placement') # Triggers cancel placment logic in Placeable of selected unit.
