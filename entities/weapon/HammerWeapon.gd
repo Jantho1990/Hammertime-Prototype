0db65b5db8926e45c3d08c1babdee1140d5f02ac
Josh Anthony <jantho1990@gmail.com>
2020-05-02 13:22:38 -0500

    starting implementation of normal melee

--- entities/weapon/HammerWeapon.gd
+++ entities/weapon/HammerWeapon.gd
@@ -6,7 +6,8 @@ enum throw_state {
   HOLDING,
   THROWING,
   SUSPENDING,
-  RETURNING
+  RETURNING,
+  MELEE
 }
 
 var current_throw_state = throw_state.HOLDING setget set_throw_state
@@ -17,6 +18,9 @@ var throwing = false
 var returning = false
 var throw_acceleration = 1
 var throw_max_speed = 1600
+var melee_range = 50
+var melee_charge_delay = 1
+var charge_delay_active = false
 var throw_range = 400
 var throw_travel_distance = 0
 var thrown_dir = dir
@@ -30,10 +34,12 @@ var __delta
 
 onready var parent = get_parent()
 onready var throwTween = $ThrowTween
+onready var MeleeChargeDelay = $MeleeChargeDelay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
   GlobalSignal.listen('teleport', self, '_on_Teleport')
+  MeleeChargeDelay.connect('timeout', self, '_on_Melee_charge_delay_timeout')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -45,7 +51,7 @@ func _physics_process(delta):
   cursor_position = get_local_mouse_position()
   # cursor_position = get_global_mouse_position()
   # GlobalSignal.dispatch('debug_label2', { 'text': dir })
-  # GlobalSignal.dispatch('debug_label', { 'text': current_throw_state })
+  GlobalSignal.dispatch('debug_label', { 'text': current_throw_state })
   # GlobalSignal.dispatch('debug_label2', { 'text': current_throw_state })
   # GlobalSignal.dispatch('debug_label2', { 'text': $ThrowTween.is_active() })
   match current_throw_state:
@@ -55,6 +61,8 @@ func _physics_process(delta):
       handle_throw_state_suspending()
     throw_state.RETURNING:
       handle_throw_state_returning()
+    throw_state.MELEE:
+      handle_throw_state_melee()
     throw_state.HOLDING, _:
      handle_throw_state_holding()
 
@@ -74,6 +82,10 @@ func handle_throw_state_holding():
   if Input.is_action_just_pressed('weapon_throw') and \
     current_throw_state == throw_state.HOLDING:
       current_throw_state = throw_state.THROWING
+  
+  if Input.is_action_just_pressed('weapon_melee') or \
+    Input.is_action_pressed('weapon_melee'):
+      current_throw_state = throw_state.MELEE
 
 func handle_throw_state_throwing():
   rotation_degrees += rotation_force_deg * dir.x
@@ -94,6 +106,16 @@ func handle_throw_state_returning():
   rotation_degrees += rotation_force_deg * thrown_dir.x
   tween_return()
 
+func handle_throw_state_melee():
+  var charging = !Input.is_action_just_pressed('weapon_melee') and \
+    !charge_delay_active
+
+  if not charging:
+    melee_normal()
+  else:
+    breakpoint
+    pass
+
 func throw_weapon():
   throwing = true
   throw_origin_position = global_position
@@ -101,16 +123,29 @@ func throw_weapon():
   throw_target_position.rotated(throw_target_position.angle() - throw_target_position.angle())
   $Sprite.rotation = 0
   # throw_target_position = (global_position + (Vector2(throw_range, 0) * thrown_dir)).rotated(global_position.angle_to_point(cursor_position))
-  var tvec = (position + cursor_position).normalized() * throw_range # * thrown_dir
-  throw_target_position = global_position + tvec
-  GlobalSignal.dispatch('debug_label', { 'text': String(tvec) + ' ' + String(cursor_position) + ' ' + String(thrown_dir) })
-  GlobalSignal.dispatch('hammer_thrown', { 'hammer': self })
+  # var tvec = (position + cursor_position).normalized() * throw_range # * thrown_dir
+  # throw_target_position = global_position + tvec
+  # GlobalSignal.dispatch('debug_label', { 'text': String(tvec) + ' ' + String(cursor_position) + ' ' + String(thrown_dir) })
+  calculate_throw_target('throw')
+  release_weapon()
   calculate_motion()
   motion = move_and_slide(motion, UP)
   
 func update_thrown_weapon():
   calculate_motion()
   motion = move_and_slide(motion, UP)
+
+func release_weapon():
+  GlobalSignal.dispatch('hammer_thrown', { 'hammer': self })
+
+func calculate_throw_target(throw_type = 'throw'):
+  var distance
+  match throw_type:
+    'throw': distance = throw_range
+    'melee':   distance = melee_range
+  
+  var tvec = (position + cursor_position).normalized() * distance
+  throw_target_position = global_position + tvec
 
 func calculate_motion():
   var motion_velocity = throw_max_speed * __delta
@@ -191,3 +226,22 @@ func update_position():
 func set_throw_state(value):
   if throw_state.has(value):
     current_throw_state = throw_state[value]
+
+func melee_normal():
+  if not throwing:
+    throwing = true
+    calculate_throw_target('melee')
+    release_weapon()
+    calculate_motion()
+  else:
+    update_thrown_weapon()
+  
+  if collision_detected() or throw_travel_distance == melee_range:
+    current_throw_state = throw_state.RETURNING
+
+func start_melee_charge_delay():
+  MeleeChargeDelay.start(melee_charge_delay)
+  charge_delay_active = true
+
+func _on_Melee_charge_delay_timeout():
+  charge_delay_active = false
